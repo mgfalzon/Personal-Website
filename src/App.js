c8ba72a32040810fa65afbef3c9c17fe81ff3324
MFalzon21 <matthew@thefalzons.com>
2020-06-05 14:18:13 -0400

    comments

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import data from './data'
 import {BrowserView, MobileView} from 'react-device-detect'
 
+// Browser
 import Header from './Components/Header'
 import Navbar from './Components/Navbar'
 import About from './Sections/About'
@@ -10,6 +11,7 @@ import Projects from './Sections/Projects'
 import Contact from './Sections/Contact'
 import {Spacer} from './Components/Various'
 
+// Mobile
 import MobileHeaderNav from './Mobile/MobileHeaderNav'
 import MobileAbout from './Mobile/MobileAbout'
 import MobileWork from './Mobile/MobileWork'
@@ -17,21 +19,19 @@ import MobileProjects from './Mobile/MobileProjects'
 import MobileContact from './Mobile/MobileContact'
 
 
-/** 
-<BrowserView>
-  <Header />
-  <Navbar />
-  <About data={data['about']}/>
-  <Work  data={data['work']} />
-  <Projects data={data['projects']}/>
-  <Spacer className='p-3' />
-  <Contact />
-  <Spacer className='p-4' />
-</BrowserView>
-**/
 
 const App = () => (
   <>
+    <BrowserView>
+      <Header />
+      <Navbar />
+      <About data={data['about']}/>
+      <Work  data={data['work']} />
+      <Projects data={data['projects']}/>
+      <Spacer className='p-3' />
+      <Contact />
+      <Spacer className='p-4' />
+    </BrowserView>
     <MobileView>
       <MobileHeaderNav />
       <MobileAbout data={data['about']}/>
