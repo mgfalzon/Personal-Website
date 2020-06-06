81f917362c6c8eba45f98245d1dc8ddae4558975
mgfalzon <matthew@thefalzons.com>
2020-06-05 23:03:55 -0400

    Updates

---
+++ precache-manifest.bbc20069f86ec967754e133ff7923b1b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "442e535d65177512ea7cc84a59eebf58",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a351395184de1c365d01",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "128af8430bba19ff3581",
+    "url": "/portfolio/static/css/main.ee9b4caf.chunk.css"
+  },
+  {
+    "revision": "a351395184de1c365d01",
+    "url": "/portfolio/static/js/2.b7c2f3ee.chunk.js"
+  },
+  {
+    "revision": "a929e4c35621369695e0145e8abf42af",
+    "url": "/portfolio/static/js/2.b7c2f3ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "128af8430bba19ff3581",
+    "url": "/portfolio/static/js/main.1100afd7.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "8ab1e663fd25be5dc6003cecc48ba273",
+    "url": "/portfolio/static/media/blinkResume.8ab1e663.PNG"
+  },
+  {
+    "revision": "2fb0457095927d83f805c8b03b1c9ccc",
+    "url": "/portfolio/static/media/headshot.2fb04570.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Personal-Website/precache-manifest.5250f48c301551210f7516720d464cfe.js"
+  "/portfolio/precache-manifest.bbc20069f86ec967754e133ff7923b1b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Personal-Website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
